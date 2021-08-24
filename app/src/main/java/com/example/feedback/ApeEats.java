f943ffe2b2c66e018676119645ae8a5879db64f1
Piyumi-Hettiarachchi <it20020880@my.sliit.lk>
2021-08-25 00:12:05 +0530

    initial commit

---
+++ app/src/main/java/com/example/feedback/ApeEats.java
@@ -0,0 +1,14 @@
+package com.example.feedback;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ApeEats extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ape_eats);
+    }
+}
