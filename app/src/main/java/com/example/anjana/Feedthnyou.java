40a0d4041c1a86d1b19f62eeb96286a35d5a32f4
Piyumi-Hettiarachchi <it20020880@my.sliit.lk>
2021-08-25 00:34:37 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/anjana/Feedthnyou.java
@@ -0,0 +1,14 @@
+package com.example.anjana;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Feedthnyou extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedthnyou);
+    }
+}
