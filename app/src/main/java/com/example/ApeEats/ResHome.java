dda805c6626c2e4b3cdd9ba9287171ea61559369
Piyumi-Hettiarachchi <it20020880@my.sliit.lk>
2021-09-23 12:41:39 +0530

    Adding New UI

---
+++ app/src/main/java/com/example/ApeEats/ResHome.java
@@ -0,0 +1,14 @@
+package com.example.ApeEats;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ResHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_res_home);
+    }
+}
