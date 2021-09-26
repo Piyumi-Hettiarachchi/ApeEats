6aa7296fe24feceb85987deb4db6d52101e2aec1
Piyumi-Hettiarachchi <it20020880@my.sliit.lk>
2021-09-26 17:08:52 +0530

    new Commit

--- app/src/main/java/com/example/ApeEats/ResHome.java
+++ app/src/main/java/com/example/ApeEats/ResHome.java
@@ -1,14 +1,36 @@
 package com.example.ApeEats;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.navigation.NavigationView;
+
 public class ResHome extends AppCompatActivity {
+
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_res_home);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+        toolbar = findViewById(R.id.toolbar);
+
+        setSupportActionBar(toolbar);
+
+        ActionBarDrawerToggle toogle = new ActionBarDrawerToggle(this,drawerLayout,toolbar,R.string.navigation_drawer_open,R.string.navigation_drawer_close);
+        drawerLayout.addDrawerListener(toogle);
+        toogle.syncState();
+
+
+
     }
 }
